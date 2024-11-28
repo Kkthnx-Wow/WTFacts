8f39d0bdbd63e930526cc7604cdf8aede7199aa0
Kkthnx <40672673+Kkthnx@users.noreply.github.com>
2024-11-28 06:46:25 -0500

    Globals and add per event settings.

--- WTFacts/Config/Settings.lua
+++ WTFacts/Config/Settings.lua
@@ -1,5 +1,8 @@
 local _, namespace = ...
 
+local string_format = string.format
+
+-- Register Settings with Event Options
 namespace:RegisterSettings("WTFactsDB", {
 	{
 		key = "enabled",
@@ -18,7 +21,7 @@ namespace:RegisterSettings("WTFactsDB", {
 		maxValue = 60,
 		valueStep = 1,
 		valueFormat = function(value)
-			return string.format("%d min", value)
+			return string_format("%d min", value)
 		end,
 	},
 	{
@@ -31,7 +34,42 @@ namespace:RegisterSettings("WTFactsDB", {
 		maxValue = 5,
 		valueStep = 1,
 		valueFormat = function(value)
-			return string.format("%d sec", value)
+			return string_format("%d sec", value)
 		end,
+	},
+	{
+		key = "achievementEarned",
+		type = "toggle",
+		title = "Fact-worthy Achievements",
+		tooltip = "Get a random WTFact when you earn an achievement.",
+		default = true,
+	},
+	{
+		key = "bossKill",
+		type = "toggle",
+		title = "Boss Down Facts",
+		tooltip = "Drop a random WTFact when you or your group defeats a boss.",
+		default = true,
+	},
+	{
+		key = "mythicPlusCompleted",
+		type = "toggle",
+		title = "Mythic+ Fact Finish",
+		tooltip = "Celebrate Mythic+ completions with a random WTFact.",
+		default = true,
+	},
+	{
+		key = "dungeonCompletion",
+		type = "toggle",
+		title = "Dungeon Done Facts",
+		tooltip = "Reveal a random WTFact after finishing a dungeon.",
+		default = true,
+	},
+	{
+		key = "levelUp",
+		type = "toggle",
+		title = "Level Up Laughter",
+		tooltip = "Share a fun WTFact to celebrate leveling up.",
+		default = true,
 	},
 })
--- WTFacts/WTFacts.lua
+++ WTFacts/WTFacts.lua
@@ -1,13 +1,28 @@
 -- Addon Table
 local _, namespace = ...
+
+-- Cache Globals and Functions for Performance
+local math_floor = math.floor
+local math_random = math.random
+local string_format = string.format
+local C_PartyInfo_IsPartyWalkIn = C_PartyInfo.IsPartyWalkIn
+local C_Timer_After = C_Timer.After
+local IsInGroup = IsInGroup
+local IsInRaid = IsInRaid
+local IsPartyLFG = IsPartyLFG
+local SendChatMessage = SendChatMessage
+local time = time
+
 local WTFacts = namespace:CreateFrame("Frame")
 
 -- Cooldown Tracker
 local lastFactTime = 0
 
+-- Utility Functions
+
 -- Function to get the appropriate chat channel
 local function GetChatChannel()
-	if IsPartyLFG() or C_PartyInfo.IsPartyWalkIn() then
+	if IsPartyLFG() or C_PartyInfo_IsPartyWalkIn() then
 		return "INSTANCE_CHAT"
 	elseif IsInRaid() then
 		return "RAID"
@@ -21,13 +36,15 @@ end
 -- Function to format time
 local function FormatTime(seconds)
 	if seconds >= 60 then
-		local mins = math.floor(seconds / 60)
+		local mins = math_floor(seconds / 60)
 		local secs = seconds % 60
-		return string.format("%d min%s %d sec%s", mins, mins > 1 and "s" or "", secs, secs > 1 and "s" or "")
+		return string_format("%d min%s %d sec%s", mins, mins > 1 and "s" or "", secs, secs > 1 and "s" or "")
 	else
-		return string.format("%d sec%s", seconds, seconds > 1 and "s" or "")
+		return string_format("%d sec%s", seconds, seconds > 1 and "s" or "")
 	end
 end
+
+-- Main Logic
 
 -- Function to display a random fact
 local function ShowRandomFact()
@@ -42,47 +59,74 @@ local function ShowRandomFact()
 	local timeLeft = cooldown - (currentTime - lastFactTime)
 
 	if timeLeft > 0 then
-		print(string.format("|cff5bc0be[WTFacts]: The addon is on cooldown. %s remaining.|r", FormatTime(timeLeft)))
+		print(string_format("|cff5bc0be[WTFacts]: The addon is on cooldown. %s remaining.|r", FormatTime(timeLeft)))
 		return
 	end
 
 	lastFactTime = currentTime
-	local randomFact = namespace.factslist[math.random(#namespace.factslist)]
+	local randomFact = namespace.factslist[math_random(#namespace.factslist)]
 	local channel = GetChatChannel()
 
-	print(string.format("|cff5bc0be[WTFacts]: Sending fact in %d seconds...|r", delay))
-	C_Timer.After(delay, function()
+	print(string_format("|cff5bc0be[WTFacts]: Sending fact in %d seconds...|r", delay))
+	C_Timer_After(delay, function()
 		SendChatMessage("[WTFacts]: " .. randomFact, channel)
 	end)
 end
 
--- Register Events
-WTFacts:RegisterEvent("ACHIEVEMENT_EARNED", ShowRandomFact)
-WTFacts:RegisterEvent("BOSS_KILL", ShowRandomFact)
-WTFacts:RegisterEvent("CHALLENGE_MODE_COMPLETED", ShowRandomFact)
-WTFacts:RegisterEvent("LFG_COMPLETION_REWARD", ShowRandomFact)
-WTFacts:RegisterEvent("PLAYER_LEVEL_UP", ShowRandomFact)
+-- Dynamic Event Registration
+
+local function UpdateEventRegistration()
+	WTFacts:UnregisterAllEvents()
+
+	if namespace:GetOption("achievementEarned") then
+		WTFacts:RegisterEvent("ACHIEVEMENT_EARNED", ShowRandomFact)
+	end
+	if namespace:GetOption("bossKill") then
+		WTFacts:RegisterEvent("BOSS_KILL", ShowRandomFact)
+	end
+	if namespace:GetOption("mythicPlusCompleted") then
+		WTFacts:RegisterEvent("CHALLENGE_MODE_COMPLETED", ShowRandomFact)
+	end
+	if namespace:GetOption("dungeonCompletion") then
+		WTFacts:RegisterEvent("LFG_COMPLETION_REWARD", ShowRandomFact)
+	end
+	if namespace:GetOption("levelUp") then
+		WTFacts:RegisterEvent("PLAYER_LEVEL_UP", ShowRandomFact)
+	end
+end
+
+-- Register Callbacks for Settings
+namespace:RegisterOptionCallback("enabled", function(value)
+	UpdateEventRegistration()
+end)
+
+namespace:RegisterOptionCallback("cooldown", function(value) end)
+
+namespace:RegisterOptionCallback("delay", function(value) end)
+
+namespace:RegisterOptionCallback("achievementEarned", function(value)
+	UpdateEventRegistration()
+end)
+
+namespace:RegisterOptionCallback("bossKill", function(value)
+	UpdateEventRegistration()
+end)
+
+namespace:RegisterOptionCallback("mythicPlusCompleted", function(value)
+	UpdateEventRegistration()
+end)
+
+namespace:RegisterOptionCallback("dungeonCompletion", function(value)
+	UpdateEventRegistration()
+end)
+
+namespace:RegisterOptionCallback("levelUp", function(value)
+	UpdateEventRegistration()
+end)
 
 -- Slash Command for Manual Trigger
 namespace:RegisterSlash("/wtfacts", function()
 	ShowRandomFact()
-end)
-
--- Register Callbacks for Settings
-namespace:RegisterOptionCallback("enabled", function(value)
-	if value then
-		print("|cff5bc0beWTFacts Enabled!|r")
-	else
-		print("|cff5bc0beWTFacts Disabled!|r")
-	end
-end)
-
-namespace:RegisterOptionCallback("cooldown", function(value)
-	print(string.format("|cff5bc0beWTFacts Cooldown set to %d minutes.|r", value))
-end)
-
-namespace:RegisterOptionCallback("delay", function(value)
-	print(string.format("|cff5bc0beWTFacts Message Delay set to %d seconds.|r", value))
 end)
 
 -- On Addon Loaded
@@ -92,9 +136,5 @@ end
 
 -- On Player Login
 function namespace:OnLogin()
-	if namespace:GetOption("enabled") then
-		print("|cff5bc0beWTFacts is enabled.|r You can configure it via the settings panel.")
-	else
-		print("|cff5bc0beWTFacts is currently disabled.|r Enable it via the settings panel.")
-	end
+	namespace:Defer(UpdateEventRegistration)
 end
